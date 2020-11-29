09c7b21070cce33512a8e61583db316611ad5a89
Cristopher Gonzalez <1719110585@utectulancingo.edu.mx>
2020-11-29 02:12:45 +0000

    CAMBIOS EN REPL.IT

--- Tienda/tienda.php
+++ Tienda/tienda.php
@@ -1,50 +1,54 @@
 <!DOCTYPE html>
-<html lang="es">
-  <head>
-    <script type="text/javascript" src="js/carrito.js" charset="utf-8"></script>
-    <title>TIENDA</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  </head>
-  <body>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="carrito.js"></script>
+	 <link rel="stylesheet" type="text/css" href="bulma.css">
+	<title>Carrio de compras</title>
+</head>
+<body>
+  <form action="ticket.php" method="GET" target="_blank">
     <div class="container">
-      <h2 style="color: PURPLE" onclick="changeColor(this.parentNode)">TIENDA DONDE LAS MEJORES MARCAS A EXCELENTES PRECIOS</h2>
-      <form method="POST" action="ticket.php" target="_blank">
-        <select name="productos" class="custom-select mb-3" id="producto">
-          <option selected>PRODUCTOS DISPONIBLES</option>
-          <option value="25">JABON DOVE 100 g</option>
-          <option value="9">LATA CHILES LA COSTEÑA 25 g</option>
-          <option value="15">SALVO ARRANCA GRASA 1/2 l</option>
-          <option value="5">TRIDENT 9 PASTILLAS</option>
-          <option value="3">SAZONADOR 2 g RIKO POLLO</option>
-          <option value="19">JABON ZOTE 22 g </option>
-          <option value="9">JUGO MAGI 12 g</option>
-          <option value="38">PAQUETE SOPAS MODERNA 69 g</option>
-        </select>  
-        <label for="cant">CANTIDAD QUE NECESITAS:</label>
-        <input type="number" class="form-control" id="cantidad" placeholder=" CANTIDA QUE NECESITAS" name="cantidad">
-        <br><h2>CARRITO</h2><br>
-        <textarea class="form-control" rows="5" id="texta" name="texta" ></textarea>
-        <br>
-        <br>
-        <label for="total">TOTAL:</label>
-        <input type="text" value="0" id="total" name="total">
-        <br>
-        <br>
-        <button type="button" class="btn btn-primary btn-lg" onclick="limpiar()">NUEVA COMPRA</button>
-        <button type="button" class="btn btn-primary btn-lg" onclick="insertar()">INSERTAR</button> 
-        <br>
-        <label for="pago">PAGO:</label>
-        <input type="number" class="form-control" id="pago" placeholder="INGRESA LA CANTIDAD DE PAGO" name="pago">
-        <br>
-        <input type="submit" class="btn btn-info" value="PAGAR">
-<body style="background-color:rgb(208, 65, 39);">
-</body>
+      <div class="colums">
+        <div class="colums is-4">
+          <h1 class="title is-1">Papeleria Jh</h1>
+          <label class="label">Cantidad</label>
+          <input type="number" name="cantidad_1" id="cantidad1" class="input" min="1" max="10">
+          <p class="help">Minimo un producto</p>
+          <label class="label">Productos</label>
+          <div class="select is-primary">
+            <select class="select" name="producto_1" id="producto1">
+                          <option>-Selecciona un producto-</option>
+                          <option value="100">Audifono Bluethoo sencillo $100.00</option>
+                          <option value="35">Cable v8 blindado $35.00</option>
+                          <option value="190">Audifono Bluethoo Airpod $190.00</option>
+                          <option value="17">Auxiliar 50 cm $17.00</option>
+                          <option value="25">Auxiliar 1.5 mts $25.00</option>
+                          <option value="10">Cable v8 Samsumg $10.00</option>
+                          <option value="17">Audifono Samsumg J5 $17.00</option>
+                          <option value="55">Cargador generico universal $55.00</option>
+                      </select>
+          </div>
+          <br>
+          <button class="button is-primary" onclick="agregar()" id="agregar1" name="agregar_1">Agregar</button>
+          <br>
+          <p class="help">Si desea hacer una nueva compra oprima el siguiente boton</p>
+          <button class="button is-primary" onclick="nueva()" id="nuevo1" name="nuevo_1">Nueva Compra</button>
+          <label class="label">Carrito de compras</label>
+          <textarea class="textarea" name="carritocompras_1" id="carritocompras1"></textarea>
+          <label class="label">Total</label>
+          <input type="number" name="total_1" id="total1" class="input" value="0" readonly>
+          <label class="label">Pago</label>
+          <input type="number" name="pago_1" id="pago1" class="input" min="1">
+          <button type="submit" class="button is-secundary">Cobrar</button>
+        </div>
+      </div>
+    </div>
   </form>
-</div>
 </body>
-</html> 
+</html>
